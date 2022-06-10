511ced5e0006b9722cdf1111f6ee7dee60bac866
arif nur rahman <arnu12rahman@gmail.com>
2022-06-10 08:57:53 +0700

    challenge promise

--- promise.js
+++ promise.js
@@ -1,8 +1,26 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+//asyc handler with inputan emosi
+const promiseOutput = async (req) => {
+    try {
+        //get data from theater promise
+        const theaIXX = await promiseTheaterIXX();
+        const theaVGC = await promiseTheaterVGC();
+
+        //merge result
+        const theaMerge = [...theaIXX, ...theaVGC];
+
+        //filter result array
+        return (
+            theaMerge?.filter((theather) => theather.hasil == req)?.length || 0
+        );
+    } catch (error) {
+        console.log("Error occur: " + error);
+    }
+};
 
 module.exports = {
-  promiseOutput,
+    promiseOutput,
 };
